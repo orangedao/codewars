5637be5c2a565c410f3cf378cb07b31096e70de2
Sergey <orangedao86@gmail.com>
2017-08-26 01:34:16 +0300

    add new file

---
+++ Welcome to the City.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''Create a method sayHello/say_hello/SayHello that takes as input a name, city,
+and state to welcome a person. Note that name will be an array consisting of one
+or more values that should be joined together with one space betweeen each, and
+the length of the name array in test cases will vary.
+
+Example:
+say_hello(['John', 'Smith'], 'Phoenix', 'Arizona')
+This example will return the string
+Hello, John Smith! Welcome to Phoenix, Arizona!'''
+
+
+def say_hello(name, city, state):
+    # your code here
+    pass
+
+
+def main():
+    say_hello()
+
+
+if __name__ == '__main__':
+    main()
